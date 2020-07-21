6a7166f2df7253a6c4855b303b5edd1ed26d4f6c
jinsheng <typistw@163.com>
2020-07-21 22:14:29 +0800

    树

---
+++ src/main/java/com/data/structure/chapter4/TreeNode.java
@@ -0,0 +1,18 @@
+package com.data.structure.chapter4;
+
+/*
+ * @author：jinsheng
+ * @date：2020/07/18 15:05
+ */
+public class TreeNode {
+
+    Object element;
+    /**
+     * 指向儿子节点
+     */
+    TreeNode firstChild;
+    /**
+     * 指向兄弟节点
+     */
+    TreeNode nextSibling;
+}
