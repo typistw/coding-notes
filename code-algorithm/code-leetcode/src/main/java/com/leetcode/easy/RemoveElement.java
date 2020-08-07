fe6dca189418d60f35a3133b89b0a307dcb4bec2
jinsheng.wei <jinsheng.wei@szzjcx.com>
2020-08-07 09:44:07 +0800

    remove element

--- code-algorithm/code-leetcode/src/main/java/com/leetcode/easy/RemoveElement.java
+++ code-algorithm/code-leetcode/src/main/java/com/leetcode/easy/RemoveElement.java
@@ -1,15 +1,88 @@
 package com.leetcode.easy;
 
+import com.alibaba.fastjson.JSON;
+
 /*
+ * Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+ *
+ * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+ *
+ * The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+ *
+ * Example 1:
+ *
+ * Given nums = [3,2,2,3], val = 3,
+ *
+ * Your function should return length = 2, with the first two elements of nums being 2.
+ *
+ * It doesn't matter what you leave beyond the returned length.
+ * Example 2:
+ *
+ * Given nums = [0,1,2,2,3,0,4,2], val = 2,
+ *
+ * Your function should return length = 5, with the first five elements of nums containing 0, 1, 3, 0, and 4.
+ *
+ * Note that the order of those five elements can be arbitrary.
+ *
+ * It doesn't matter what values are set beyond the returned length.
+ * Clarification:
+ *
+ * Confused why the returned value is an integer but your answer is an array?
+ *
+ * Note that the input array is passed in by reference, which means modification to the input array will be known to the caller as well.
+ *
+ * Internally you can think of this:
+ *
+ * // nums is passed in by reference. (i.e., without making a copy)
+ * int len = removeElement(nums, val);
+ *
+ * // any modification to nums in your function would be known by the caller.
+ * // using the length returned by your function, it prints the first len elements.
+ * for (int i = 0; i < len; i++) {
+ *     print(nums[i]);
+ * }
  * @author：jinsheng
  * @date：2019/04/27 23:31
  */
 public class RemoveElement {
     public static void main(String[] args) {
-
+        int[] num = {2,2,3,3}, num2 = {0,1,2,2,3,0,4,2};
+        int val =3 , val2 = 2;
+        removeElement(num, val);
+        removeElement(num2, val2);
+        removeElement2(num, val);
+        removeElement2(num2, val2);
     }
 
     private static int removeElement(int[] nums, int val){
-        return 0;
+        int count = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == val){
+                for(int j = nums.length - 1; j > i; j--){
+                    if(nums[j] != val){
+                        nums[i] = nums[j];
+                        nums[j] = val;
+                        count++;
+                        break;
+                    }
+                }
+            }else{
+                count ++;
+            }
+        }
+        System.out.println(JSON.toJSONString(nums) + " : " + count);
+        return count;
+    }
+
+    private static int removeElement2(int[] nums, int val){
+        int count = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != val) {
+                nums[count] = nums[i];
+                count ++;
+            }
+        }
+        System.out.println(JSON.toJSONString(nums) + " : " + count);
+        return count;
     }
 }
