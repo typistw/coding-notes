be23e41327358a1f69c8d8009ccd1e7b0a54f480
jinsheng.wei <jinsheng.wei@szzjcx.com>
2020-08-28 19:04:29 +0800

    sql

---
+++ code-algorithm/code-leetcode/src/main/java/com/leetcode/easy/DuplicateEmails.sql
@@ -0,0 +1,18 @@
+# Write a SQL query to find all duplicate emails in a table named Person.
+#
+# +----+---------+
+# | Id | Email   |
+# +----+---------+
+# | 1  | a@b.com |
+# | 2  | c@d.com |
+# | 3  | a@b.com |
+# +----+---------+
+# For example, your query should return the following for the above table:
+#
+# +---------+
+# | Email   |
+# +---------+
+# | a@b.com |
+# +---------+
+
+select r.Email from (select Email, count(1) sum from Person group by Email having sum > 1) r ;
